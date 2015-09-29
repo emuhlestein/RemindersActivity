16a9f1678e657a3c2782a1497ec2d00ef7bde821
Ed Muhlestein <emuhlestein@yahoo.com>
2015-09-29 16:42:32 -0500

    Add arrayadapter for list. Add dummy data.

--- app/src/main/java/com/intelliviz/remindersactivity/RemindersActivity.java
+++ app/src/main/java/com/intelliviz/remindersactivity/RemindersActivity.java
@@ -4,13 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class RemindersActivity extends Activity {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
+
+        mListView = (ListView)findViewById(R.id.reminders_list_view);
+        // The ArrayAdapter is the controller in our model-view-controller
+        // relationship. (controller)
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, // context
+                R.layout.reminders_row, // layout (View)
+                R.id.row_text, // (view)
+                // data (model) test data to test listview
+                new String[]{"First record", "second record", "third record"});
+        mListView.setAdapter(arrayAdapter);
     }
 
     @Override
